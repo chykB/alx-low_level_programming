8e32f47067b9e3b5a1afd7ff8ff07a002051a9be
chykB <malikchika86@gmail.com>
2022-12-09 13:31:18 -0800

    This program prints the alphabet in lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - print the alphabet in lowercase letters
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char letter:
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	printf("\n")
+	return (0);
+}
