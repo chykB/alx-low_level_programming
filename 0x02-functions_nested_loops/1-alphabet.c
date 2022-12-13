23b53192f6fae1ef0d5f6f299ad2d873f2bbd2ee
chykB <malikchika86@gmail.com>
2022-12-13 08:34:12 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 /**
  * print_alpahbet - print all alphabet in lower case
  *
- * Return: always 0
 */
 void print_alphabet(void);
 {
@@ -11,5 +10,4 @@ void print_alphabet(void);
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	_putchar('\n');
-	return (0);
 }
