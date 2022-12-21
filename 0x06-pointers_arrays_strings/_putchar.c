58941605f77bd616bf8fdb30b13a5567b50534ac
chykB <malikchika86@gmail.com>
2022-12-21 07:17:07 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the charcter
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is return
+*/
+int _putchar(char c)
+{
+	return(write(1,&c,1));
+}
