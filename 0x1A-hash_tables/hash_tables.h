81dee5b3100aeb03b9a7d273fc135635f8c745b9
chykB <malikchika86@gmail.com>
2023-04-07 02:40:35 -0700

    Updated

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,9 @@
-#ifndef HASH_TABLE_H
-#define HASH_TABLE_H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
