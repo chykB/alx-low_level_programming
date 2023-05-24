e5844b39b5419d7efff529d90f7b0c3c05b69bdc
chykB <malikchika86@gmail.com>
2023-05-24 15:12:18 -0700

    a function def island_perimeter(grid): that returns the perimeter of the island described in grid.

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    a function def island_perimeter(grid): that returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (list): A list of lists representing the grid with 0s and 1s.
+    Returns:
+        Int: The perimeter of the island.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
